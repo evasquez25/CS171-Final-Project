1df1a7cf370ee7bed976c2d597c20205ef9749ae
Joshua Zhang <joshua_zhang@college.harvard.edu>
2024-11-25 05:32:20 -0500

    josh changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // Function to convert date objects to strings or reverse
+import { newsDeskDescriptions } from "../data/newsDeskDescriptions.js";
+
 let dateFormatter = d3.timeFormat("%Y-%m-%d");
 let dateParser = d3.timeParse("%Y-%m-%d");
 let lineGraph;
@@ -12,3 +14,12 @@ d3.csv("data/randomized_data.csv", row => row).then(displayData => {
 document.getElementById('news-desk-selector').addEventListener('change', () => {
     wordCloud.wrangleData(); // Re-filter and update the word cloud
 });
+
+document.getElementById('news-desk-description-selector')
+    .addEventListener('change', function() {
+        const selectedValue = this.value;
+        const description = newsDeskDescriptions.find(item => Object.keys(item)[0] === selectedValue);
+        if (description) {
+            document.getElementById('news-desk-description').textContent = description[selectedValue];
+        }
+    });
